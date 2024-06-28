8ae499cfbc650da74e1eb291916569cf7ecb43fd
Preetam Chinde <75744062+prem1601@users.noreply.github.com>
2024-06-28 17:01:44 +0530

    Completed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
+
+    console.log('Connecting to DB...');
+
     // Check the connection status
     if (mongoose.connection.readyState === 1) {
       console.log("Already connected to MongoDB");
@@ -14,6 +17,7 @@ async function connectDB() {
     console.log('MongoDB connected...');
   } catch (error) {
     console.log(error);
+    console.log('Connection failed!, Check above error in console...');
     process.exit(1);
   }
 }
